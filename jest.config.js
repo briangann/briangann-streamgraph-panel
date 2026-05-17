b6d8cee3d4a8a456a1323466d61cc4aedcf06e9f
Brian Gann <bkgann@gmail.com>
2026-05-17 09:47:38 -0400

    chore: add D3 dependencies and extend jest ESM transform config

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,19 @@
 // generally used by snapshots, but can affect specific tests
 process.env.TZ = 'UTC';
 
+const { grafanaESModules, nodeModulesToTransform } = require('./.config/jest/utils');
+
+const additionalESModules = [
+  'd3-array',
+  'd3-axis',
+  'd3-scale',
+  'd3-selection',
+  'd3-shape',
+  'd3-time',
+  'd3-time-format',
+];
+
 module.exports = {
-  // Jest configuration provided by Grafana scaffolding
   ...require('./.config/jest.config'),
+  transformIgnorePatterns: [nodeModulesToTransform([...grafanaESModules, ...additionalESModules])],
 };
